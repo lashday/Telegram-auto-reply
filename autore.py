1d168b0b84d6a110e8e73bd884581689d3ff5008
lashday <78397718+lashday@users.noreply.github.com>
2021-02-02 19:07:52 +0800

    Update autore.py

--- autore.py
+++ autore.py
@@ -3,17 +3,19 @@ import asyncio
 import time
 from telethon import events
 
-api_id = 'put your api_id here'
-api_hash = 'put yout api_hash here'
+api_id = '2905565'
+api_hash = '10e132995f08087650dccbf2614df267'
 
-client = TelegramClient('Username', api_id, api_hash)
+client = TelegramClient('lashdaysg', 2905565, 10e132995f08087650dccbf2614df267)
 
-message = 'Hey, I will be at a retreat until Saturday. I promise to get in touch as soon as I can. See you.'
+message = '💐Hi there! Thank you for your message.
+❤️If you'd like to book an appointment, please go to https://lashdaysg.setmore.com for instant confirmation. (Price list is in there too)
+👩‍🦱For enquiries/appointments, we will respond as soon as possible. Thank you!'
 
 def main():
 
     client.start()
-    
+
     @client.on(events.NewMessage(incoming=True))
     async def _(event):
         if event.is_private:
